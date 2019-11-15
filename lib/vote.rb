1a4703969b69d34b0d8b5088a2b91909000f2d9d
singingcactus <bechma02@gmail.com>
2019-11-15 12:04:00 -0500

    who knows?

--- lib/vote.rb
+++ lib/vote.rb
@@ -4,8 +4,7 @@ class Vote < ActiveRecord::Base
 	belongs_to :bill
 
 	def position_status(lawmaker)
-        puts "\n#{lawmaker.full_name}'s position was #{self.vote} on #{self.bill.slug}."
-	end
-	
+		puts "\n#{lawmaker.full_name}'s position was #{self.vote} on #{self.bill.slug}."
+	end	
 	
 end
